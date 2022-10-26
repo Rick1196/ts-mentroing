bea201f55799cade783d0cf58c063085f11ba000
Rick1196 <ricardompp11@gmail.com>
2022-10-25 22:14:15 -0500

    basics v2

--- index.ts
+++ index.ts
@@ -8,6 +8,7 @@ const exampleString = "string example";
 
 const exampleArray = [];
 exampleArray.push(1);
+exampleArray.push("string");
 
 const exampleObject = {};
 exampleObject["name"] = "Example";
@@ -30,9 +31,10 @@ exampleObject2["name"] = "ricardo";
 exampleObject2.addres = "Example address";
 
 type Person = {
-  name?: string;
+  name?: string; // optional attribute
   addres: string;
   age?: number;
+  bloodType?: string;
   exampleFunction?: (age: number) => void;
   contact?: {
     name: string;
@@ -48,10 +50,81 @@ const examplePerson: Person = {
 examplePerson.exampleFunction(10);
 //undefined
 const result = examplePerson.age + 10;
-const contactName = examplePerson.contact.name;
+// const contactName = examplePerson.contact.name;
 console.log("Result Age", result / 10);
 // Person = {} => error
 // arrays
 const exampleArray2: Person[] = [];
 exampleArray2.push({ addres: null });
+exampleArray2.push({ addres: null, name: "Example" });
 // exampleArray2.push(1); => Error
+//sync
+//[1,2,3,4]
+// for(let i = 0; i < array.length; i ++){
+// const newVar = array[i];
+// }
+// for(const item of exampleArray2){
+//   const name = item.name;
+//   console.log('Name',name)
+// }
+
+//async
+// exampleArray2.forEach((person, index) =>{
+//   console.log(person, index);
+// })
+
+const realPerson: Person = {
+  addres: "Gatito",
+  age: 10,
+  contact: {
+    name: "",
+    phone: "",
+  },
+  name: "",
+  bloodType: "o+",
+};
+// realPerson.age;
+// realPerson['age']
+
+// Object
+// key => string
+for (const key of Object.keys(realPerson)) {
+  console.log(key, realPerson[key]);
+}
+// operadores binarios && -> and || => or ! => not
+// operadores aritmeticos ===, <==, >==, <, >, !==
+// if(){}if else () {}else{}
+// switch (key) {
+//   case value:
+
+//     break;
+
+//   default:
+//     break;
+// }
+
+const num = 10;
+// if (num % 2 === 0) {
+//   console.log(num, "par");
+// } else if (num % 3 === 0) {
+//   console.log(num, "multiplo de 3");
+// } else {
+//   console.log(num, "ninguno");
+// }
+
+// if (num % 3 === 0 && num % 2 !== 0) {
+//   console.log(num, "multiplo de 3");
+// } else {
+//   console.log(num, "ninguno");
+// }
+
+// if (num % 2 === 0 || num % 5 === 0) {
+//   console.log(num, "multiplo de 3");
+// } else {
+//   console.log(num, "ninguno");
+// }
+const defaultStreet = undefined;
+
+const personAddress =
+  (realPerson.addres || defaultStreet || "default" ) + " street";
+console.log(personAddress);
