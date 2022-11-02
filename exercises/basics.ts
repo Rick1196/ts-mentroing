07b391e36f6e5f81facf883b03b372c871c4fc49
Rick1196 <ricardompp11@gmail.com>
2022-11-01 18:00:41 -0600

    exercise basics

---
+++ exercises/basics.ts
@@ -0,0 +1,30 @@
+// exercise 1
+// create a type Product with the next properties
+/**
+ * name: string => name of the product
+ * price: number => price of the product
+ * categories: string[] => name of the categories which the product belongs to example: 'food', 'decoration', 'cars'
+ * disscount?:number  => optional field that indicates if the product has a discont and how much is the discount
+ */
+// create an array with 10 or more items of type Product each product must have at least one of the following categories: 
+/**
+ * books
+ * software
+ * videogames
+ * toys
+ * clothes
+ */
+
+// create a variable of type array of Product and save all the products on the first array that has a disscount
+//  create a variable of type array of Product and save all the products on the first array that has the category of software or videogames
+
+// exercise 2
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * example
+ * array =  [2,7,11,15]
+ * target = 9
+ * 
+ * indices = [0, 1]
+ * because array[0] is 2 and array[1] is 7, so 2 + 7 = 9
+ */
